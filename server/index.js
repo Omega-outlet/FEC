ae1ced364168b4e3f82c69fdff27934af44a761d
Kimberly Tom <kimberlyjtom@gmail.com>
2023-08-10 16:21:06 -0700

    added express

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
+
+
+app.listen(process.env.PORT);
+console.log(`Listening at http://localhost:${process.env.PORT}`);
