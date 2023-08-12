078e32f568b9b0535890104cb6611068d7316644
Eric Chang <fsc8504@gmail.com>
2023-08-12 12:38:03 -0700

    Pass the state as props to the QuestionsList component

---
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import QuestionsList from './QuestionsList.jsx';
+import dummyData from './dummyData.js';
+
+function QuestionsAndAnswers() {
+  const [questions, setQuestions] = useState(dummyData.results);
+  return (
+    <div>
+      <QuestionsList questions={questions} />
+    </div>
+  );
+}
+
+export default QuestionsAndAnswers;
