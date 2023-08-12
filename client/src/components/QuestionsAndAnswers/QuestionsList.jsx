1492d36c16ce0ef8c3892cb767716aa66f0fcf2d
Eric Chang <fsc8504@gmail.com>
2023-08-12 16:48:41 -0700

    Add more Q & A list details and sort questions in order of helpfulness

--- client/src/components/QuestionsAndAnswers/QuestionsList.jsx
+++ client/src/components/QuestionsAndAnswers/QuestionsList.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { sortByHelp } from '../../../utils/sortHelp.js'
+import { reFormatDate } from '../../../utils/reFormatDate.js';
 
 function QuestionsList({ questions }) {
-  const defaultFourQuestions = questions.slice(0, 4);
+  const sortedQuestions = sortByHelp(questions);
+  const defaultFourQuestions = sortedQuestions.slice(0, 4);
 
   return (
     <div>
@@ -10,12 +13,15 @@ function QuestionsList({ questions }) {
         {defaultFourQuestions.map((question) => (
           <li key={question.question_id}>
             <p>Who asked the question?</p>
-            <p>{`Asked by ${question.asker_name}`}</p>
+            <p>{`Asked by ${question.asker_name}, ${reFormatDate(question.question_date)}` }</p>
+            <h4>Q</h4>
+            <p>{question.question_body}</p>
+            <p>Helpful?</p>
             <ul>
               {Object.values(question.answers).slice(0, 2).map((answer) => (
                 <li key={answer.id}>
                   <p>{answer.body}</p>
-                  <p>{`Answered by ${answer.answerer_name}`}</p>
+                  <p>{`Answered by ${answer.answerer_name}, ${reFormatDate(answer.date)}`}</p>
                 </li>
               ))}
             </ul>
@@ -29,9 +35,12 @@ function QuestionsList({ questions }) {
 QuestionsList.propTypes = {
   questions: PropTypes.arrayOf(PropTypes.shape({
     question_id: PropTypes.number.isRequired,
+    question_body: PropTypes.string.isRequired,
+    question_date: PropTypes.string.isRequired,
     asker_name: PropTypes.string.isRequired,
     answers: PropTypes.objectOf(PropTypes.shape({
       id: PropTypes.number.isRequired,
+      data: PropTypes.string.isRequired,
       body: PropTypes.string.isRequired,
       answerer_name: PropTypes.string.isRequired,
     })).isRequired,
