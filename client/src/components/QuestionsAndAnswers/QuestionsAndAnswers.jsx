00c76a616741f1c64866c29b7915f83378f23faf
Eric Chang <fsc8504@gmail.com>
2023-08-16 16:11:20 -0700

    Add Get request

--- client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import QuestionsList from './QuestionsList/QuestionsList.jsx';
-import dummyData from './dummyData.js';
 import SearchBar from './Buttons/SearchBar.jsx';
 import { QAContainer } from './styled-components/QuestionsAndAnswers.styles.jsx';
 
-function QuestionsAndAnswers() {
-  const [questions, setQuestions] = useState(dummyData.results);
-  const [filteredQuestions, setFilteredQuestions] = useState(questions);
+function QuestionsAndAnswers(currentProductID) {
+  const [questions, setQuestions] = useState([]);
+  const [filteredQuestions, setFilteredQuestions] = useState([]);
+
+  const productId = 40325;
+  useEffect(() => {
+    axios.get('/questions', {
+      params: {
+        product_id: productId,
+        count: 100,
+      },
+    })
+      .then((res) => {
+        setQuestions(res.data.results);
+        setFilteredQuestions(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const handleSearch = (query) => {
     if (query) {
