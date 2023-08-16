5c33c12e46b76583b632d92e0a6820db042d7dfa
Eric Chang <fsc8504@gmail.com>
2023-08-16 16:08:11 -0700

    Add q & a controller and route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,4 @@
 exports.products = require('./product.js');
 exports.overView = require('./overViewController.js');
+exports.ratings = require('./ratingsController.js');
+exports.questions = require('./questionsController.js');
--- server/router.js
+++ server/router.js
@@ -6,4 +6,6 @@ const controller = require('./controllers');
 router.get('/api/product', controller.products.getProduct);
 router.get('/api/product/styles', controller.overView.getProductStyles);
 
+router.get('/questions', controller.questions.getQuestions);
+
 module.exports = router;
