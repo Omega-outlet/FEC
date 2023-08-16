24c81bfe4599b5427490a513450965773432fab3
Lauren Laxton <laurenlaxton1@gmail.com>
2023-08-16 14:21:53 -0700

    add new controller to routes

--- server/router.js
+++ server/router.js
@@ -6,5 +6,6 @@ const controller = require('./controllers');
 router.get('/api/product', controller.products.getProduct);
 router.get('/api/product/styles', controller.overView.getProductStyles);
 router.get('/api/product/related', controller.relatedProducts.getRelatedProducts);
+router.get('/api/product/relatedStyle', controller.relatedProductStyle.getRelatedStyle);
 
 module.exports = router;
