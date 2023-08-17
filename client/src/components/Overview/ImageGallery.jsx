98163c637e8583cdb5f2ce0d83de21c06c40f516
Kimberly Tom <kimberlyjtom@gmail.com>
2023-08-16 22:43:28 -0700

    renders style's first image to image gallery

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,11 +1,38 @@
-import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
+import Promise from 'bluebird';
+import ImageGalleryComponents from '../../styled-components/overviewcomponents/image-gallery-components.jsx';
 
-function ImageGallery({ currentProduct, selectedStyle }) {
-  // console.log("selected style in image gallery", selectedStyle);
+function ImageGallery({ selectedStyle }) {
+  const [mainImage, setMainImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const getStyle = () => {
+    function getMainImage() {
+      setIsLoading(true);
+      let didSucceed = false;
+      return new Promise((resolve, reject) => {
+        if (typeof selectedStyle !== 'undefined') {
+          didSucceed = true;
+        }
+        if (didSucceed === true) {
+          resolve(selectedStyle.photos[0].url);
+        } else {
+          reject(new Error('Not done loading'));
+        }
+      });
+    }
+
+    getMainImage()
+      .then((data) => { setMainImage(data); })
+      .catch(() => {})
+      .finally(setIsLoading(false));
+};
+
+  useEffect(getStyle, [selectedStyle, mainImage]);
   return (
     <div>
       <h1>Image Gallery</h1>
+      {isLoading ? null : (<ImageGalleryComponents.MainPhoto src={mainImage} />)}
+      {/* <img src={mainImage} display="block"></img> */}
 
     </div>
   );
---
+++ client/src/styled-components/overviewcomponents/image-gallery-components.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const MainPhoto = styled.img`
+  display: block;
+  width:400px;
+  height:500px;
+  object-fit: cover;
+`;
+
+export default { MainPhoto };
