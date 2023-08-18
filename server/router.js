fcbec291798905dedd30eec41f98fdc8b20eaea3
Eric Chang <fsc8504@gmail.com>
2023-08-18 14:18:27 -0700

    Update controller and router for report

--- server/router.js
+++ server/router.js
@@ -11,5 +11,6 @@ router.get('/questions', controller.questions.getQuestions);
 router.get('/reviews', controller.ratings.getRatings);
 
 router.put('/qa/:type/:id/helpful', controller.questions.updateHelpful);
+router.put('/qa/:type/:id/report', controller.questions.reportQA);
 
 module.exports = router;
