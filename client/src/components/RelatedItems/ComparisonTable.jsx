53f79aed5d28cf4722eb214dca2d86d4ab164f9f
Lauren Laxton <laurenlaxton1@gmail.com>
2023-08-19 19:34:08 -0700

    add get request for product features

--- client/src/components/RelatedItems/ComparisonTable.jsx
+++ client/src/components/RelatedItems/ComparisonTable.jsx
@@ -1,21 +1,23 @@
 //create style component for table design
 import React from 'react';
 
-const ComparisonTable = function ({currentProduct, currentStyle, comparedProduct, comparedStyle}) {
-  const tableRows = <tr><td>{currentProduct.slogan}</td><td>Sample comparison row</td><td>{comparedProduct.slogan}</td></tr>;
+const ComparisonTable = function ({currentProduct, comparedProduct}) {
 
-  //make th position: sticky
+  //Create a comparison object
+
+  //map to rows
+  const tableRows = () => (
+  <tr>
+    <td>{currentProduct.slogan}</td><td>Comparison category</td><td>{comparedProduct.slogan}</td>
+  </tr>
+  );
+
   return (
     <table>
       <thead>
-        <tr>
-          <th>Comparing</th>
-          <th></th>
-          <th></th>
-        </tr>
-        <tr>
+        <tr style={{ position: 'sticky' }}>
           <th>{currentProduct.name}</th>
-          <th></th>
+          <th>Comparing</th>
           <th>{comparedProduct.name}</th>
         </tr>
       </thead>
@@ -27,24 +29,3 @@ const ComparisonTable = function ({currentProduct, currentStyle, comparedProduct
 };
 
 export default ComparisonTable;
-
-/* <table>
-  <thead>
-    <tr class="red">
-      <th>Name</th>
-      <th>Age</th>
-      <th>Job</th>
-      <th>Color</th>
-      <th>URL</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Lorem.</td>
-      <td>Ullam.</td>
-      <td>Vel.</td>
-      <td>At.</td>
-      <td>Quis.</td>
-    </tr>
-  </tbody>
-</table> */
