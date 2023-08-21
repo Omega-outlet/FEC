525df54bfc3d69985a452a8acd04118fd3ef386b
Eric Chang <fsc8504@gmail.com>
2023-08-20 17:09:20 -0700

    Update yes and report style

--- client/src/styled-components/YesAndReportButton.styles.jsx
+++ client/src/styled-components/YesAndReportButton.styles.jsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
 export const YesAndReportButton = styled.button`
+  margin: 0;
+  padding: 0;
   background: none;
   border: none;
   font-family: var(--body-text-font-family,inherit);
   color: #069;
   text-decoration: underline;
   cursor: pointer;
-
+  line-height: 2;
   &:hover {
     text-decoration: none;
   }
