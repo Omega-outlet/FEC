f98520d8d01be1775062464cf94b5a8f883063d1
Eric Chang <fsc8504@gmail.com>
2023-08-21 18:59:10 -0700

    Add Max height for the question list to scroll, infinite scroll and spinner animation

--- client/src/components/QuestionsAndAnswers/styled-components/QuestionsAndAnswers.styles.jsx
+++ client/src/components/QuestionsAndAnswers/styled-components/QuestionsAndAnswers.styles.jsx
@@ -1,15 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const QAContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  border-top: 1px solid;
   border-bottom: 1px solid;
+  max-height: 100vh;
 `;
 
 export const QuestionList = styled.ul`
   list-style-type: none;
+  max-height: 80vh;
+  overflow-y: auto;
 `;
 
 export const QuestionDetailsList = styled.li`
@@ -22,6 +24,7 @@ export const QuestionDetailsList = styled.li`
   margin-bottom: 15px;
   border-bottom: var(--common-border-width,1px) solid var(--common-border-color,rgba(0,0,0,0.15));
   padding-bottom: 40px;
+
 `;
 
 export const AskerDetailsContainer = styled.div`
@@ -55,6 +58,11 @@ export const AnswerListContainer = styled.ul`
 export const SearchBarContainer = styled.div`
   width: 65%;
   padding: 20px;
+  position: sticky;
+  // makes it stick on top of the scrollable section
+  top: 0;
+  background-color: white;
+  padding-bottom: 0;
 `;
 
 export const SearchInput = styled.input`
@@ -77,6 +85,7 @@ export const LoadMoreAndAddNewButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
   gap: 0.5rem;
+  padding-bottom: 10px;
 `;
 
 export const BodyAndQuestionContainer = styled.div`
@@ -86,4 +95,18 @@ export const BodyAndQuestionContainer = styled.div`
 export const QuestionAskedByText = styled.p`
   font-size: 10px;
   color: #0E1311;
+`;
+const spinAnimation = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+export const ScrollSpinner = styled.div`
+  border: 16px solid whitesmoke;
+  border-top: 16px solid dodgerblue;
+  border-radius: 50%;
+  width: 15px;
+  height: 15px;
+  animation: ${spinAnimation} 2s linear infinite;
+  // make it center
+  margin: 20px auto;
 `;
