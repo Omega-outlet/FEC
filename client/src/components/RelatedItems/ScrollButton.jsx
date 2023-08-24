1b9ee5089c7e26dd2ed4f424473815fd1e7bfebc
Lauren Laxton <laurenlaxton1@gmail.com>
2023-08-24 00:09:48 -0700

    minor css tweaks

--- client/src/components/RelatedItems/ScrollButton.jsx
+++ client/src/components/RelatedItems/ScrollButton.jsx
@@ -2,12 +2,11 @@ import React, { useContext } from 'react';
 import { StyledButton } from '../../styled-components/common-elements.jsx';
 import ThemeContext from '../ThemeContext.jsx';
 
-const ScrollButton = function ({ scroll, dir, vis }) {
+const ScrollButton = function ({ scroll, dir }) {
   const { theme } = useContext(ThemeContext);
 
   return <StyledButton onClick={scroll} $theme={theme} style={{
     'border-radius': '0',
-    'visibility': {vis},
     'transparency': '0.8',
   }}>{dir === 'left' ? '<' : '>'}</StyledButton>;
 };
